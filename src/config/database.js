acf94517edfea77f078a30ea157f45e8ec35291a
???? <samrsd893@gmail.com>
2026-06-22 01:46:45 +0200

    zz

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,25 @@ const dns = require('dns');
 const env = require('./env');
 const logger = require('./logger');
 
+const legacyIndexesToDrop = {
+  apartments: ['ownerId_1_address_1_buildingNumber_1_floor_1_unitNumber_1'],
+};
+
+async function dropLegacyIndexes(connection) {
+  await Promise.all(Object.entries(legacyIndexesToDrop).map(async ([collectionName, indexNames]) => {
+    const collection = connection.db.collection(collectionName);
+    const indexes = await collection.indexes();
+    const existingNames = new Set(indexes.map((index) => index.name));
+
+    await Promise.all(indexNames
+      .filter((indexName) => existingNames.has(indexName))
+      .map(async (indexName) => {
+        await collection.dropIndex(indexName);
+        logger.info({ collection: collectionName, index: indexName }, 'Dropped legacy MongoDB index');
+      }));
+  }));
+}
+
 async function connectDatabase(uri = env.mongoUri) {
   mongoose.set('strictQuery', true);
   if (uri.startsWith('mongodb+srv://') && env.mongoDnsServers.length > 0) {
@@ -13,6 +32,7 @@ async function connectDatabase(uri = env.mongoUri) {
     maxPoolSize: 20,
     serverSelectionTimeoutMS: 10000,
   });
+  await dropLegacyIndexes(mongoose.connection);
   logger.info('MongoDB connected');
 }
 
